b39551f7b35dffbb1670de743b259fc503360909
Your Name <shubhampathak1996@gmail.com>
2022-05-27 15:35:55 +0530

    update

--- src/containers/case-study/AllCaseStudies.js
+++ src/containers/case-study/AllCaseStudies.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Footer from '../../components/common/Footer'
+import { BLOG, CASESTUDY } from '../../shared/models/casestudy'
 import Header from '../../components/common/Header'
 import PageBanner from '../../components/common/PageBanner'
 
@@ -12,72 +13,27 @@ function AllCaseStudies() {
       <section className="case-study ptb-50">
         <div className="container">
           <div className="row">
-            <div className="col-md-4">
-              <Link to={`/case-study/single`}>
-                <div className="single-case-study">
-                  <img src="https://luhaifdigitech.com/images/case-study/app-intro.png" />
-                  <div className="case-study-content">
-                    <h3>DERMAPURITYS</h3>
-                    <h4>SERVICES: DIGITAL MARKETING, LEAD GENERATION</h4>
+            {CASESTUDY &&
+              CASESTUDY.map((item) => {
+                return (
+                  <div className="col-md-4">
+                    <a href={`${item.link}`} target="_blank" className="pdf-i">
+                      <div className="single-case-study">
+                        <img src={item.image} />
+                        <div className="case-study-content">
+                          <h3>{item.title}</h3>
+                          <h4>{item.subtitle}</h4>
+                        </div>
+                        <div className="bg-pdf">
+                          <div className="content-pdf">
+                            <i class="fa fa-file-pdf-o"></i>
+                          </div>
+                        </div>
+                      </div>
+                    </a>
                   </div>
-                </div>
-              </Link>
-            </div>
-            <div className="col-md-4">
-              <a href>
-                <div className="single-case-study">
-                  <img src="https://luhaifdigitech.com/images/case-study/ngo.PNG" />
-                  <div className="case-study-content">
-                    <h3>DERMAPURITYS</h3>
-                    <h4>SERVICES: DIGITAL MARKETING, LEAD GENERATION</h4>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4">
-              <a href>
-                <div className="single-case-study">
-                  <img src="https://luhaifdigitech.com/images/case-study/pgx.PNG" />
-                  <div className="case-study-content">
-                    <h3>DERMAPURITYS</h3>
-                    <h4>SERVICES: DIGITAL MARKETING, LEAD GENERATION</h4>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4">
-              <a href>
-                <div className="single-case-study">
-                  <img src="https://luhaifdigitech.com/images/case-study/luhaif-d.PNG" />
-                  <div className="case-study-content">
-                    <h3>DERMAPURITYS</h3>
-                    <h4>SERVICES: DIGITAL MARKETING, LEAD GENERATION</h4>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4">
-              <a href>
-                <div className="single-case-study">
-                  <img src="https://luhaifdigitech.com/images/case-study/app-intro.png" />
-                  <div className="case-study-content">
-                    <h3>DERMAPURITYS</h3>
-                    <h4>SERVICES: DIGITAL MARKETING, LEAD GENERATION</h4>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <div className="col-md-4">
-              <a href>
-                <div className="single-case-study">
-                  <img src="https://luhaifdigitech.com/images/case-study/app-intro.png" />
-                  <div className="case-study-content">
-                    <h3>DERMAPURITYS</h3>
-                    <h4>SERVICES: DIGITAL MARKETING, LEAD GENERATION</h4>
-                  </div>
-                </div>
-              </a>
-            </div>
+                )
+              })}
           </div>
         </div>
       </section>
---
+++ src/shared/models/casestudy.js
@@ -0,0 +1,23 @@
+export const CASESTUDY = [
+  {
+    image: 'https://luhaifdigitech.com/images/case-study/app-intro.png',
+    title: 'DERMAPURITYS',
+
+    subtitle: 'Skin Care Clinic',
+    link: 'https://luhaifdigitech.com/case_study/Case_Study _derma.pdf',
+  },
+  {
+    image: 'https://luhaifdigitech.com/images/case-study/app-intro.png',
+    title: 'DERMAPURITYS',
+
+    subtitle: 'Skin Care Clinic',
+    link: 'https://luhaifdigitech.com/case_study/Case_Study _derma.pdf',
+  },
+  {
+    image: 'https://luhaifdigitech.com/images/case-study/app-intro.png',
+    title: 'DERMAPURITYS',
+
+    subtitle: 'Skin Care Clinic',
+    link: 'https://luhaifdigitech.com/case_study/Case_Study _derma.pdf',
+  },
+]
